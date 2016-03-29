dc19a46188fae5421b847d3557cf4aca651d5f57
AnonymousX1 <uni.vlad@inbox.lv>
2016-03-29 18:16:45 +0300

    ololok

--- core/src/main/java/com/dteam/cookapi/controllers/IndexController.java
+++ core/src/main/java/com/dteam/cookapi/controllers/IndexController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/")
 public class IndexController {
-
+    //defs
     @RequestMapping(method = RequestMethod.GET)
     public String index(){
         return "index";
